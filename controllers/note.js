ba0966fbb9b29b0286571b9ef81d09385fb8e53f
Jordan Boggs <boggsjp@gmail.com>
2018-03-31 10:27:13 -0600

    doing MVC correctly

--- controllers/note.js
+++ controllers/note.js
@@ -1,1 +1,32 @@
-// I'm using Vue which follows MVVM architecture
+// Require all models
+const db = require('../models');
+
+module.exports = {
+  // Post a Note associated with a Headline
+  postNote: function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Headline.findOneAndUpdate({
+        _id: req.params.id,
+      }, {
+        $push: {
+          notes: dbNote._id
+        }
+      }, {
+        new: true
+      });
+    })
+    .then(function(dbHeadline) {
+      res.json(dbHeadline);
+    })
+    .catch((err) => res.json(err));
+  },
+  // Delete a Note
+  deleteNote: function(req, res) {
+    db.Note.deleteOne({
+      _id: req.params.id
+    })
+    .then((dbNotes) => res.json(dbNotes))
+    .catch((err) => res.json(err));
+  }
+}
