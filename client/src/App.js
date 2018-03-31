dd747397dc0534de6ef066957d96a188550a2ecc
Jordan Boggs <boggsjp@gmail.com>
2018-03-31 11:07:09 -0600

    created and display header

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, 
          Route, 
          Switch } from "react-router-dom";
+import Header from "./components/Header"
 
 const App = () => (
-  <h1>Hi</h1>
+  <Header />
 );
 
 export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="navbar navbar-dark bg-primary">
+    <h1 className="navbar-brand">News Scraper</h1>
+    <button className="btn btn-secondary" id="scraper">Scrape</button>
+  </header>
+)
+
+export default Header;
