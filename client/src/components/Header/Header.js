7af1c5b35198cec4b3ad8722907e730f17238712
Jordan Boggs <boggsjp@gmail.com>
2018-04-01 10:34:33 -0600

    container inside nav

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="navbar navbar-dark bg-primary">
-    <h1 className="navbar-brand">News Scraper</h1>
-    <button className="btn btn-secondary" id="scraper">Scrape</button>
+  <header className="navbar navbar-fluid navbar-dark bg-primary">
+    <div className="container">
+      <h1 className="navbar-brand">News Scraper</h1>
+      <button className="btn btn-secondary" id="scraper">Scrape</button>
+    </div>
   </header>
 )
 
